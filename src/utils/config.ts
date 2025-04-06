068074a0cbd0660a1dbba146bbce560c6174fc0b
Scr Hz <ohesmondhalgh@gmail.com>
2025-04-06 12:37:53 +0100

    Setup prettier & vs code repo settings

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,11 @@
 export default (environment) => {
     return {
-        accessToken: process.env.CONTENTFUL_PREVIEW === 'true' ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken:
+            process.env.CONTENTFUL_PREVIEW === 'true'
+                ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+                : process.env.CONTENTFUL_ACCESS_TOKEN,
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         environment,
-        host: process.env.CONTENTFUL_PREVIEW === 'true' ? 'preview.contentful.com' : 'cdn.contentful.com'
-    }
-}
+        host: process.env.CONTENTFUL_PREVIEW === 'true' ? 'preview.contentful.com' : 'cdn.contentful.com',
+    };
+};
